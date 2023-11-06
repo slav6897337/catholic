502af71fb1438a1d526ccb9508967a6538d8569f
slav6897337 <slava6897337@gmail.com>
2023-11-06 19:11:46 +0100

    feat(News):sort by date as default

--- Catholic.Api/Apis/NewsApi.cs
+++ Catholic.Api/Apis/NewsApi.cs
@@ -10,7 +10,7 @@ public static class NewsApi
     public static void MapNewsApi(this IEndpointRouteBuilder endpoints)
     {
         endpoints.MapGet("/api/news/all", (NewsService newsService) =>
-            newsService.GetAllAsync());
+            newsService.GetAllAsync(sortBy: n => n.Date, ascending: false));
         
         endpoints.MapGet("/api/news", (NewsService newsService, [AsParameters] RequestQuery request) =>
             newsService.GetPagingAsync(request.Paging(), request.HolyMassOnly is true ? f => f.IsChurchNews : null));
--- Catholic.Domain/News.cs
+++ Catholic.Domain/News.cs
@@ -5,7 +5,6 @@ public class News : Entity
     public string? Link { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
-    
     public bool IsChurchNews { get; set; }
     public bool IsHomeNews { get; set; } = true;
 }
